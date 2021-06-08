5d134c3c9334385cf14a49f258a70772a21cd96f
pradeepbyreddy <byreddypradeep.sai@gmail.com>
2021-06-09 01:13:50 +0530

    First commit

---
+++ prime.py
@@ -0,0 +1,10 @@
+for number in range(2,15):
+    print("number is " + str(number))
+    for x in range(2, number):
+        print("x is " + str(x))
+        if number % x == 0:
+            print(number, 'equals', x, '*', number//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(number, 'is a prime number')
