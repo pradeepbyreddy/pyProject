9fabc9e1686de0d1c24af57af4926351b81d506e
pradeepbyreddy <byreddypradeep.sai@gmail.com>
2021-06-09 01:10:44 +0530

    First commit

---
+++ pyttest.py
@@ -0,0 +1,16 @@
+import sys
+import logging
+import os
+
+
+def feb(n):
+	a, b = 0,1
+	result=[]
+	while(a<n):
+		#print(a, end = " ")
+		result += [ a ] #result.append(a)
+		a,b = b, a+b
+	return result
+
+feb=feb(23)
+print(feb)
